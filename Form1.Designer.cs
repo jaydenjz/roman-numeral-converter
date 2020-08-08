0ef442367f61545f67f3a330afc536eb4039bd2e
Jayden Fam <fam.jiangzhao@stee.stegg.com>
2020-08-08 09:19:43 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RomanNumeralConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.result = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(314, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 104);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CONVERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(296, 26);
+            this.input.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.result);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 72);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.result.Location = new System.Drawing.Point(6, 22);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(18, 20);
+            this.result.TabIndex = 0;
+            this.result.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 123);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Roman Numeral Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomanNumeralConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(input.Text, out int textInput))
+            {
+                result.Text = NumberToRoman(textInput);
+                return;
+            }
+            result.Text = RomanToNumber(input.Text);
+            input.Text = input.Text.ToUpper();
+        }
+
+        // convert number to roman
+        private string NumberToRoman(int number)
+        {
+            string roman = "";
+            if(number < 0 || number > 3999)
+            {
+                return "Please enter a value between 0 to 3999";
+            }
+            foreach(var pair in GetRomanNumeralPair())
+            {
+                while(number >= pair.Value)
+                {
+                    number -= pair.Value;
+                    roman += pair.Key;
+                }
+            }
+            return roman;
+        }
+
+        // convert roman to number
+        private string RomanToNumber(string roman)
+        {
+            int number = 0;
+            int temp = 0;
+            int lesserCount = 0;
+            int previous = 0;
+
+            if(roman == null)
+            {
+                return "Input can not be null.";
+            }
+
+            // standardize the roman input
+            roman = roman.ToUpper().Trim();
+
+            if(!IsValidRoman(roman))
+            {
+                return "Invalid Roman Numeral";
+            }
+
+            // loop trough the roman input by each character
+            for(int i = 0; i < roman.Length; i++)
+            {
+                int current = GetNumberByRoman(roman[i]);
+
+                // to check if left side lower than right
+                if(previous < current)
+                {
+                    lesserCount++;
+                }
+
+                // each roman lesser than previous more than 2 time subsequently 
+                if(lesserCount > 2)
+                {
+                    return "Invalid Roman Numeral";
+                }
+
+                // prevent input like IVI, CMC, CMCD
+                if(temp*-1 == current)
+                {
+                    return "Invalid Roman Numeral";
+                }
+
+                // not end of string; current number less than next number (eg.IV);
+                if(i + 1 < roman.Length && current < GetNumberByRoman(roman[i + 1]))
+                {
+                    if("IXC".IndexOf(roman[i]) == -1 || GetNumberByRoman(roman[i + 1]) > (current * 10) || roman.Split(roman[i]).Length > 3)
+                    {
+                        return "**Invalid Roman Numeral**";
+                    }
+                    number -= current;
+                    temp = number;
+                }
+                else
+                {
+                    number += current;
+                    lesserCount--;
+                }
+                previous = current;
+            }
+
+            return number.ToString();
+        }
+
+        private int GetNumberByRoman(char roman)
+        {
+            if(GetRomanNumeralPair().TryGetValue(roman.ToString(), out int number))
+            {
+                return number;
+            }
+            return 0;
+        }
+
+        private bool IsValidRoman(string input)
+        {
+            int count = 1;
+            char temp = 'Z';
+            foreach(char numeral in input)
+            {
+                // check if numeral is valid
+                if(!GetRomanNumeralPair().TryGetValue(numeral.ToString(), out _))
+                {
+                    Console.WriteLine("Invalid roman character(s).");
+                    return false;
+                }
+
+                // check if numeral duplicated (eg. IIII is not valid)
+                if(numeral == temp)
+                {
+                    count++;
+                    if(count == 4)
+                    {
+                        Console.WriteLine("Roman character(s) repeated more than 3 times.");
+                        return false;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                    temp = numeral;
+                }
+            }
+
+            // invalid combination check
+            if(input.Split('V').Length > 2 || input.Split('L').Length > 2 || input.Split('D').Length > 2)
+            {
+                Console.WriteLine("Invalid roman combination.");
+                return false;
+            }
+
+            return true;
+        }
+
+        // a list of roman and its relative numeral
+        private Dictionary<string, int> GetRomanNumeralPair()
+        {
+            return new Dictionary<string, int>
+            {
+                { "M", 1000 },
+                { "CM", 900},
+                { "D", 500 },
+                { "CD", 400},
+                { "C", 100 },
+                { "XC", 90 },
+                { "L", 50 },
+                { "XL", 40 },
+                { "X", 10 },
+                { "IX", 9 },
+                { "V", 5 },
+                { "IV", 4 },
+                { "I", 1 }
+            };
+        }
+    }
+}
